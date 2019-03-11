c09925cdf0fdb6fe85df659451a06df3671d025c
arnavkartikeya <38819180+arnavkartikeya@users.noreply.github.com>
2019-03-11 14:18:27 -0700

    Update Mushroom.java

--- Mushroom.java
+++ Mushroom.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Mushroom here.
  * 
- * @author Mehul Tailang 
+ * @author Arnav Kartikeya
  * @version 1.0
  */
 public class Mushroom extends Actor
